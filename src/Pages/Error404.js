b30cc725d0d8d702dc4cbf661611aa0fee4a2294
Ophelie Bellissens <obellissens1@gmail.com>
2023-05-31 18:14:17 +0200

    Page error 404

--- src/Pages/Error404.js
+++ src/Pages/Error404.js
@@ -1,8 +1,19 @@
 import React from "react";
+import "./styles/Error.css"
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 const Error = () => {
     return(
-        <p>Error 404</p>
+    <div>
+        <Header />
+        <main>
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <a href="/"> Retourner sur la page d'accueil</a>
+        </main>
+        <Footer />
+    </div>
     )
 }
 
