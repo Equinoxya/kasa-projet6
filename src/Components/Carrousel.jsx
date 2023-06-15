dc14e8a87bbe1582590d3335cc567a3692ed05c6
Ophelie Bellissens <obellissens1@gmail.com>
2023-06-15 14:23:36 +0200

    CSS page appart

---
+++ src/Components/Carrousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles/carrousel.css"
+
+const Carrousel = () => {
+    return (
+        <div className='carrousel'>
+            
+            
+        </div>
+    );
+};
+
+export default Carrousel;
